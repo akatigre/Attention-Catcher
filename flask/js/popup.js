e614649c5f7d2fd8348e7b9ad2ee7f6a897dba9e
Erin <65602256+Soyeon-ErinLee@users.noreply.github.com>
2020-08-27 19:12:58 +0900

    updated

--- flask/js/popup.js
+++
@@ -1,8 +0,0 @@
-/*globals $:false */
-$(function() {
-	"use strict";
-	$('#pop').on('click', function() {
-		$('.imagepreview').attr('src', $('#imageresource').attr('src'));
-		$('#imagemodal').modal('show');
-		});
-});
