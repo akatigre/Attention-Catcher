3bd98159da7a3e0343aab866142c3bc57a916def
Junwon Park <crboy7@naver.com>
2020-08-30 18:58:09 +0900

    update

--- ocr/trdg/run.py
+++ ocr/trdg/run.py
@@ -2,7 +2,7 @@ import argparse
 import os, errno
 import sys
 
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 print(os.getcwd())
 import random as rnd
 import string
