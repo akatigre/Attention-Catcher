6c0ed98a4b7a1a36e8c6a7a8b09e8b1678df7e81
Erin <65602256+Soyeon-ErinLee@users.noreply.github.com>
2020-09-15 19:14:30 +0900

    updated

--- 5. Flask/temp.py
+++
@@ -1,6 +0,0 @@
-from pdf2image import convert_from_path
-
-images = convert_from_path('example.pdf', poppler_path=r"poppler\bin")
-for i, image in enumerate(images):
-    fname = "image" + str(i) + ".jpg"
-    image.save(fname, "JPEG")
